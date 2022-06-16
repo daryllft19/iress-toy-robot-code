2c593b6fdd37d385f025471db19a41a1beb19ba3
Daryll Tumambing <daryll.tumambing@gmail.com>
2022-06-16 23:41:50 +0800

    Fix orientation constant name usage

--- src/models/robot/index.py
+++ src/models/robot/index.py
@@ -1,18 +1,18 @@
 from src.constants.orientation import Orientation
 
 class Robot:
-    def __init__(self, x=0, y=0, orientation=Orientation.NORTH):
+    def __init__(self, x=0, y=0, orientation=Orientation.NORTH.name):
         self.place(x, y, orientation)
 
     def __str__(self):
         val = 'R'
-        if Orientation.NORTH:
+        if self._orientation == Orientation.NORTH.name:
             val += ' ^'
-        elif Orientation.SOUTH:
+        elif self._orientation == Orientation.SOUTH.name:
             val += ' v'
-        elif Orientation.WEST:
+        elif self._orientation == Orientation.WEST.name:
             val += ' <'
-        elif Orientation.EAST:
+        elif self._orientation == Orientation.EAST.name:
             val += ' >'
 
         return val
@@ -23,6 +23,9 @@ class Robot:
     def coords(self):
         return self._x, self._y
 
+    def orientation(self):
+        return self._orientation
+
     def place(self, x, y, orientation):
         self._x = x
         self._y = y
@@ -32,31 +35,31 @@ class Robot:
         return (self._x, self._y, self._orientation)
 
     def move(self):
-        if self._orientation == Orientation.NORTH:
+        if self._orientation == Orientation.NORTH.name:
             self._y += 1
-        elif self._orientation == Orientation.SOUTH:
+        elif self._orientation == Orientation.SOUTH.name:
             self._y -= 1
-        elif self._orientation == Orientation.WEST:
+        elif self._orientation == Orientation.WEST.name:
             self._x -= 1
-        elif self._orientation == Orientation.EAST:
+        elif self._orientation == Orientation.EAST.name:
             self._x += 1
 
     def left(self):
-        if self._orientation == Orientation.NORTH:
-            self._orientation = Orientation.WEST
-        elif self._orientation == Orientation.SOUTH:
-            self._orientation = Orientation.EAST
-        elif self._orientation == Orientation.WEST:
-            self._orientation = Orientation.SOUTH
-        elif self._orientation == Orientation.EAST:
-            self._orientation = Orientation.NORTH
+        if self._orientation == Orientation.NORTH.name:
+            self._orientation = Orientation.WEST.name
+        elif self._orientation == Orientation.SOUTH.name:
+            self._orientation = Orientation.EAST.name
+        elif self._orientation == Orientation.WEST.name:
+            self._orientation = Orientation.SOUTH.name
+        elif self._orientation == Orientation.EAST.name:
+            self._orientation = Orientation.NORTH.name
 
     def right(self):
-        if self._orientation == Orientation.NORTH:
-            self._orientation = Orientation.EAST
-        elif self._orientation == Orientation.SOUTH:
-            self._orientation = Orientation.WEST
-        elif self._orientation == Orientation.WEST:
-            self._orientation = Orientation.NORTH
-        elif self._orientation == Orientation.EAST:
-            self._orientation = Orientation.SOUTH
+        if self._orientation == Orientation.NORTH.name:
+            self._orientation = Orientation.EAST.name
+        elif self._orientation == Orientation.SOUTH.name:
+            self._orientation = Orientation.WEST.name
+        elif self._orientation == Orientation.WEST.name:
+            self._orientation = Orientation.NORTH.name
+        elif self._orientation == Orientation.EAST.name:
+            self._orientation = Orientation.SOUTH.name
