1ea99936ae9da22b93b5ec49db834fafabd58d5d
Daryll Tumambing <daryll.tumambing@gmail.com>
2022-06-16 16:11:04 +0800

    Add base table methods

--- src/models/table/index.py
+++ src/models/table/index.py
@@ -1,2 +1,13 @@
 class Table:
-    pass
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+
+        self._grid = [[-1 for x in range(self._width)] for y in range(self._length)]
+
+    def __str__(self):
+        return self._grid
+
+    def display(self):
+        for y in range(self._length):
+            print(self._grid[y])
